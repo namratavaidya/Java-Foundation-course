f505c82c57bc255283ff9877f91987e9a87c5725
namratavaidya <namrata.vaidya2006@gmail.com>
2026-02-16 15:00:27 +0530

    Add StudentMain class with main method

---
+++ StudentMain.java
@@ -0,0 +1,13 @@
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.setStudent(100, "Neha","CSE");
+		s1.getStudent();
+		
+		Student s2 = new Student();
+		s2.setStudent(101, "Janu", "Finance");
+		s2.getStudent();
+	}
+
+}
