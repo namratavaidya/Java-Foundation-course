00065cb6e1e37f6bff8dc52caba8d96d603b9c54
namratavaidya <namrata.vaidya2006@gmail.com>
2026-04-16 18:08:12 +0530

    Add Student class with setter and getter methods

---
+++ 17feb.java
@@ -0,0 +1,28 @@
+public class Student {
+	private int rollno;
+	private String name;
+	private String branch;
+}
+
+public void setStudent(int rollno, String name, String branch) {
+	this.rollno = rollno;
+	this.name = name;
+	this.branch=branch;
+}
+
+public void getStudent() {
+	System.out.println("Roll Number is" +rollno);
+	System.out.println("Name is " +name);
+    System.out.println("Branch is " +branch);
+----------------------------------------------------------------------------------------------
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1 = new Student();
+		
+		s1.setStudent(100, "Abc", "CSE");
+		s1.getStudent();
+	}
+}
