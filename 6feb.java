aa24c94ec3538807fec036246d156a26bc07aa74
namratavaidya <namrata.vaidya2006@gmail.com>
2026-04-16 18:05:43 +0530

    Create 6feb.java

---
+++ 6feb.java
@@ -0,0 +1,17 @@
+
+public class StringeExample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String str = "Nagpur";
+			System.out.println(str);
+			String fname = "ABC";
+			System.out.println(fname);
+			String lname = "xyz";
+			System.out.println(lname);
+			int len = str.length();
+			System.out.println("length is : " + len);
+			String s1 = str.toUpperCase();
+			System.out.println("upper case string is : " + s1);
+	}
+}
